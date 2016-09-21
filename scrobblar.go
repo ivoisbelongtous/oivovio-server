00a16abee929b8a6e9cae34f5570246f3788d5c3
Ivo Murrell <ivoisbelongtous@gmail.com>
2016-09-21 17:04:15 +0100

    Write last song played on lastfm to HTTP response.
    API key is saved in a separate file expected to be placed next to the Go binary. Don’t want my key on GitHub tbh.

--- scrobblar.go
+++ scrobblar.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
+var apiKey string
+
 func ScrobbleServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Listen to more music!\n")
+	res, err := http.Get("http://ws.audioscrobbler.com/2.0/?" +
+		"method=user.getRecentTracks&" +
+		"api_key=" + apiKey +
+		"&user=Doomboy95" +
+		"&limit=1" +
+		"&format=json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(w, res.Body)
+	res.Body.Close()
 }
 
 func main() {
+	r, err := os.Open("apikey.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fscan(r, &apiKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/scrobbles", ScrobbleServer)
 	http.ListenAndServe(":8345", nil)
 }

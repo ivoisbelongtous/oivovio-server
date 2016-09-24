90a765536503474c468b99cb248d601824e64323
Ivo Murrell <ivoisbelongtous@gmail.com>
2016-09-24 17:30:57 +0100

    Synchronise requests to response cache
    Make use of channels (CSP!)

--- scrobblar.go
+++ scrobblar.go
@@ -10,46 +10,57 @@ import (
 	"time"
 )
 
+// Don't save API key in source.
 var apiKey string
 
+type ScrobbleStatus int
+
+const (
+	Ready ScrobbleStatus = iota
+	Fetching
+)
+
 type ScrobbleRequest struct {
-	json        string
-	requestTime time.Time
+	status ScrobbleStatus
+	json   string
 }
 
-var scrobble ScrobbleRequest
-
+// Request REST response from last.fm API.
 func GetScrobble() string {
-	// Invalidate cache after 30 seconds.
-	ttl := scrobble.requestTime.Add(time.Duration(30) * time.Second)
-	if scrobble.requestTime.IsZero() || ttl.Before(time.Now()) {
-
-		res, err := http.Get("http://ws.audioscrobbler.com/2.0/?" +
-			"method=user.getRecentTracks&" +
-			"api_key=" + apiKey +
-			"&user=Doomboy95" +
-			"&limit=1" +
-			"&format=json")
-		if err != nil {
-			log.Fatal(err)
-		}
-		var buf bytes.Buffer
-		io.Copy(&buf, res.Body)
-		res.Body.Close()
-
-		scrobble.json = buf.String()
-		resTime := res.Header.Get("Date")
-		scrobble.requestTime, err = time.Parse(time.RFC1123, resTime)
-		if err != nil {
-			log.Fatal(err)
-		}
+	res, err := http.Get("http://ws.audioscrobbler.com/2.0/?" +
+		"method=user.getRecentTracks&" +
+		"api_key=" + apiKey +
+		"&user=Doomboy95" +
+		"&limit=1" +
+		"&format=json")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return scrobble.json
+	var buf bytes.Buffer
+	io.Copy(&buf, res.Body)
+	res.Body.Close()
+	return buf.String()
 }
 
-func ScrobbleServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, GetScrobble())
+func ScrobbleMonitor(out chan ScrobbleRequest) {
+	// Cache times out after 30 seconds
+	timeout := time.After(30 * time.Second)
+
+	sr := ScrobbleRequest{Fetching, ""}
+	for {
+		select {
+		case out <- sr:
+			if sr.status == Fetching {
+				sr = ScrobbleRequest{Ready, GetScrobble()}
+				// Reset timeout
+				timeout = time.After(30 * time.Second)
+			}
+		case <-timeout:
+			// Invalidate the cache
+			sr = ScrobbleRequest{Fetching, ""}
+		}
+	}
 }
 
 func main() {
@@ -62,6 +73,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/scrobbles.json", ScrobbleServer)
+	c := make(chan ScrobbleRequest)
+	go ScrobbleMonitor(c)
+
+	http.HandleFunc("/scrobbles.json",
+		func(w http.ResponseWriter, req *http.Request) {
+			// Poll until valid scrobble returned
+			for {
+				r := <-c
+				if r.status == Ready {
+					io.WriteString(w, r.json)
+					return
+				}
+			}
+		})
 	http.ListenAndServe(":8345", nil)
 }
